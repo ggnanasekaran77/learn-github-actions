98996f8224ad44c19d4922f5ad7666fe3ea720f7
gnanasekaran.gajendi <gnanasekaran.gajendi@rakuten.com>
2022-03-07 06:40:43 +0800

    testing commit message

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,16 +6,17 @@ message=${INPUT_MESSAGE}
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
-git fetch --tags
-git fetch --prune --unshallow || true
-
 current_tag=$(git ls-remote --tags origin | tail -1 | awk -F/ '{print $3}' | awk -F^ '{print $1}')
 echo "current_tag: ${current_tag}"
 
-major=$(echo current_tag | cut -d. -f1)
-minor=$(echo current_tag | cut -d. -f2)
-patch=$(echo current_tag | cut -d. -f3)
+major=$(echo ${current_tag} | cut -d. -f1)
+minor=$(echo ${current_tag} | cut -d. -f2)
+patch=$(echo ${current_tag} | cut -d. -f3)
 echo "major: ${major} minor: ${minor} patch: ${patch}"
+
+echo "message: ${message}"
+
+git show --pretty="" --name-only -n 1
 
 git tag -a "${tag}" -m "${message}"
 git push origin "${tag}" || true
