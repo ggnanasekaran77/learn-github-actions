6e77d2a840d9401d1783b9601b16e533f22ec020
gnanasekaran.gajendi <gnanasekaran.gajendi@rakuten.com>
2022-03-07 05:57:46 +0800

    list remote tags

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,10 @@ message=${INPUT_MESSAGE}
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
+git fetch --tags
+git fetch --prune --unshallow || true
+
+git ls-remote --tags origin
+
 git tag -a "${tag}" -m "${message}"
 git push origin "${tag}"
